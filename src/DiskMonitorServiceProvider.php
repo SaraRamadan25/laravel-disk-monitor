3c98b3b7587dc4c1c73eaacabacffc9357ad5b60
SaraRamadan25 <sarahramadanelsayed@gmail.com>
2023-07-30 01:00:50 +0300

    Use PackageServiceProvider

--- src/DiskMonitorServiceProvider.php
+++ src/DiskMonitorServiceProvider.php
@@ -3,71 +3,29 @@
 namespace Spatie\LaravelDiskMonitor;
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\ServiceProvider;
 use Spatie\LaravelDiskMonitor\Commands\RecordDiskMetricsCommand;
 use Spatie\LaravelDiskMonitor\HTTP\Controllers\DiskMetricsController;
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class DiskMonitorServiceProvider extends ServiceProvider
+class DiskMonitorServiceProvider extends PackageServiceProvider
 {
-    public function boot(): void
+    public function configurePackage(Package $package): void
     {
-        $this
-            ->registerPublishable()
-            ->registerCommands()
-            ->registerRoutes()
-            ->registerViews();
+        $package
+            ->name('laravel-disk-monitor')
+            ->hasViews()
+            ->hasConfigFile()
+            ->hasMigration('create_disk_monitor_tables')
+            ->hasCommand(RecordDiskMetricsCommand::class);
     }
 
-    public function register(): void
-    {
-        $this->mergeConfigFrom(__DIR__.'/../config/disk-monitor.php', 'disk-monitor');
-    }
-
-    public function registerPublishable(): self
-    {
-        if (!$this->app->runningInConsole()) {
-            return $this;
-        }
-        $this->publishes([
-            __DIR__.'/../config/disk-monitor.php' => config_path('disk-monitor.php'),
-        ], 'config');
-
-        $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('resources/views/vendor/disk-monitor'),
-        ], 'views');
-
-        if (!class_exists('CreatePackageTables')) {
-            $this->publishes([
-                __DIR__.'/../database/migrations/create_disk_monitor_tables.php.stub'
-                => database_path('migrations/'.date('Y_m_d_His', time()).'_create_disk_monitor_tables.php'),
-            ], 'migrations');
-        }
-        return $this;
-    }
-
-    public function registerCommands(): self
-    {
-        if (!$this->app->runningInConsole()) {
-            return $this;
-        }
-        $this->commands([
-            RecordDiskMetricsCommand::class,
-        ]);
-        return $this;
-    }
-
-    public function registerRoutes(): self
+    public function packageRegistered()
     {
         Route::macro('diskMonitor', function (string $prefix) {
             Route::prefix($prefix)->group(function () {
                 Route::get('/', DiskMetricsController::class);
             });
         });
-        return $this;
-    }
-    public function registerViews(): self
-    {
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'disk-monitor');
-        return $this;
     }
 }
